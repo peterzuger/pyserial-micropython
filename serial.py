81330cee2329006472fd03b65cb53004c7950fc3
Peter Züger <zueger.peter@icloud.com>
2021-06-04 12:05:43 +0200

    added .send_break(duration) method

--- serial.py
+++ serial.py
@@ -213,6 +213,15 @@ class Serial:
             )
         self._baudrate = baudrate
 
+    def send_break(self, duration=0.25):
+        """\
+        Send break condition. Timed, returns to idle state after given
+        duration.
+        """
+        if not self.is_open:
+            raise PortNotOpenError()
+        _libc.i_tcsendbreak_ii(self.fd, int(duration / 0.25))
+
     # compatibility with io library
 
     def readable(self):
